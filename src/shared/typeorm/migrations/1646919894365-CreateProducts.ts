2014bf811c91af30bc8a86a361417b81ebe86564
kaludematto <kalu.dematto@gmail.com>
2022-03-10 13:59:55 -0300

    Created Services for Products creation

---
+++ src/shared/typeorm/migrations/1646919894365-CreateProducts.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateProducts1646919894365 implements MigrationInterface {
+  public async up(_queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
